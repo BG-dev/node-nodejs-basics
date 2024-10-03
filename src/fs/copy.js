66828a2f85749ccf76833809eacaa6292cbf7a7f
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-03 19:47:49 +0300

    feat: finish copy.js subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  const dirname = import.meta.dirname;
+  const sourcePath = path.join(dirname, "/files");
+  const destinationPath = path.join(dirname, "/files_copy");
+  try {
+    await fs.cp(sourcePath, destinationPath, { force: false, errorOnExist: true, recursive: true });
+  } catch (error) {
+    console.log(error);
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
