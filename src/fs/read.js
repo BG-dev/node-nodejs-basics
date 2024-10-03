f76d7959a98107b63eae6160bdbe57ebdac20bfe
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-03 20:32:01 +0300

    feat: finish read.js subtask

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const dirname = import.meta.dirname;
+  const fileToReadPath = path.join(dirname, "/files/fileToRead.txt");
+
+  try {
+    const file = await fs.readFile(fileToReadPath);
+    console.log(file.toString());
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
