f11644e05ea32a06479cfacf762447d4fc397a76
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-05 22:09:52 +0300

    feat: finish args.js subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const argsMap = [];
+  for (let i = 0; i < args.length; i += 2) {
+    argsMap.push([args[i].slice(2), args[i + 1]]);
+  }
+  const resultText = argsMap.reduce((msg, arg, argIndex, args) => {
+    return msg + `${arg[0]} is ${arg[1]}` + (argIndex + 1 !== args.length ? ", " : "");
+  }, "");
+  console.log(resultText);
 };
 
 parseArgs();
