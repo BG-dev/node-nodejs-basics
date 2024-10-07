b062192dda264513351cbb8e788accd2980f266e
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-07 23:22:52 +0300

    feat: finish transform.js subtask

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedText = chunk.toString().split("").reverse().join("");
+      callback(null, reversedText);
+    },
+  });
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
