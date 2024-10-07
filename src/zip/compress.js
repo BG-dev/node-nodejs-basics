ee28cf845ff24c74d90e10aa835301ec7fa7ff98
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-07 23:31:20 +0300

    feat: finish compress.js subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import * as zlib from "zlib";
+import * as path from "path";
+import * as fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompressPath = path.join(import.meta.dirname, "files/fileToCompress.txt");
+  const archivePath = path.join(import.meta.dirname, "files/archive.gz");
+
+  const readStream = fs.createReadStream(fileToCompressPath);
+  const writeStream = fs.createWriteStream(archivePath);
+
+  const gzip = zlib.createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
