e27867299270c3d1fc5b89572f1e7ffd4e9be30a
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-07 23:13:06 +0300

    feat: finish read.js subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
+import { stdout } from "process";
+
 const read = async () => {
-    // Write your code here 
+  const fileToReadPath = path.join(import.meta.dirname, "files/fileToRead.txt");
+  const readStream = fs.createReadStream(fileToReadPath);
+  readStream.pipe(process.stdout);
+
+  readStream.on("end", () => {
+    process.stdout.write("\n");
+  });
 };
 
 await read();
