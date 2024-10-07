579f428367f3b0c03828480e54768a62ef1e9f32
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-07 22:54:15 +0300

    feat: finish calcHash.js subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import * as path from "path";
+import * as fs from "fs";
+import * as crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileToCalculateHashForPath = path.join(
+    import.meta.dirname,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  const readStream = fs.createReadStream(fileToCalculateHashForPath);
+  const hash = crypto.createHash("sha256");
+
+  readStream.pipe(hash).on("finish", () => {
+    console.log("hash: " + hash.digest("hex"));
+  });
 };
 
 await calculateHash();
